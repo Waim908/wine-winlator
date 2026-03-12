dd7d178ef9eb676d77e65fdc9284e28ac7677b76
Waim908 <waimxj449@foxmail.com>
2026-03-12 21:16:54 +0800

    Script: Add wcp-package.sh

---
+++ wcp-package.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+create_json () {
+if [[ -z $customDescription ]]; then
+cat > '/tmp/output-wcp/profile.json' << EOF
+{
+  "type": "Wine",
+  "versionName": "${wine-ver}",
+  "versionCode": 1,
+  "description": "${wine-ver}-tkg-stg-ge. Built form [https://github.com/Waim908/wine-winlator]",
+  "files": [],
+  "wine": {
+          "binPath": "bin",
+          "libPath": "lib",
+          "prefixPack": "prefixPack.txz"
+  }
+}
+EOF
+else
+cat > '/tmp/output-wcp/profile.json' << EOF
+{
+  "type": "Wine",
+  "versionName": "${wine-ver}",
+  "versionCode": 1,
+  "description": "${customDescription}",
+  "files": [],
+  "wine": {
+          "binPath": "bin",
+          "libPath": "lib",
+          "prefixPack": "prefixPack.txz"
+  }
+}
+EOF
+fi
+}
+rm -rf /data/data/com.winlator/files/imagefs/home/xuser/.wine
+rm -rf /tmp/output-whp
+rm -rf /data/data/com.winlator/files/imagefs/tmp
+mkdir -p /data/data/com.winlator/files/imagefs/tmp/
+mkdir -p /data/data/com.winlator/files/imagefs/home/xuser/.wine
+if [[ -z $wineVer ]]; then
+  echo "你必须声明wineVer变量"
+  exit 1
+fi
+export WINEESYNC=1
+export WINEPREFIX=/data/data/com.winlator/files/imagefs/home/xuser/.wine
+export winePath=$1/bin
+export wineRoot=$1
+if ! command -v zstd; then
+  echo "zstd未安装"
+  exit 1
+fi
+if ! command -v xz; then
+  echo "xz未安装"
+  exit 1
+fi
+if [[ -z $winePath ]]; then
+  echo "没有在参数1定义wine可执路径"
+  exit 1
+else
+  export USER=xuser
+  if [[ $useBox64 == 1 ]]; then
+      echo "使用box64执行"
+      box64 $winePath/wineboot || exit 1
+  else
+      $winePath/wineboot || exit 1
+  fi
+  sleep 3
+  exit_status=$?
+  if [[ $exit_status -eq 0 ]]; then
+      echo "wineboot 执行成功"
+  else
+      echo "wineboot 执行失败，退出码: $exit_status"
+      # 这里可以添加失败处理逻辑
+      exit 1
+  fi
+fi
+sleep 2
+if [[ $useBox64 == 1 ]]; then
+  wine_version=$(box64 $winePath/wine --version)
+else
+  wine_version=$($winePath/wine --version)
+fi
+[[ $doNotCreateTXT == 1 ]] || {
+cat > '/data/data/com.winlator/files/imagefs/home/xuser/.wine/drive_c/ProgramData/Microsoft/Windows/Start Menu/TkG-version.txt' << EOF
+Version: $wine_version
+Others:
+  More staging settings in winecfg
+  [Waim908/wine-winlator](https://github.com/Waim908/wine-winlator)
+EOF
+}
+rm -rf $WINEPREFIX/dosdevices/*
+# if [[ haveInclude == 1 ]]
+timeStamp=$(TZ=Asia/Shanghai date +%s)
+if [[ $useTimestamp == 1 ]]; then
+  # 实际使用可能会强制替换为disable ，也就是说这个意义不大，仅作确认更新时间用
+  echo $timeStamp > $WINEPREFIX/.update-timestamp
+else
+  # 这里参考了longjunyu的包，里面是disable
+  echo "disable" > $WINEPREFIX/.update-timestamp
+fi
+cd $WINEPREFIX/..
+rm -rf .wine/dosdevice/z:
+mkdir -p /tmp/output-wcp
+tar -I 'xz -T$(nproc) -9' -cvf /tmp/output-whp/prefixPack.txz .wine
+cp -r -p $wineRoot/bin /tmp/output-wcp/
+cp -r -p $wineRoot/lib /tmp/output-wcp/
+cp -r -p $wineRoot/share /tmp/output-wcp/
+cd /tmp/output-whp/
+create_json
+if [[ -z $customWcpName ]]; then
+  tar -I 'zstd -T$(nproc) -9' -cvf /tmp/output-whp/wine-$wineVer.wcp bin/ lib/ share/ prefixPack.txz profile.json
+else
+  tar -I 'zstd -T$(nproc) -9' -cvf /tmp/output-whp/$customWcpName.wcp bin/ lib/ share/ prefixPack.txz profile.json
+fi
+echo "Output=> /tmp/output-whp/wine-$wineVer.wcp"
