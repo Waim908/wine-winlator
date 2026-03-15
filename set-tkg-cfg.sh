ee4858e18c5ae4f7b06e93632a631359a52250bd
Waim908 <waimxj449@foxmail.com>
2026-03-15 18:37:03 +0800

    set-tkg-cfg.sh: Del --disable-winemenubuilder  arg

--- set-tkg-cfg.sh
+++ set-tkg-cfg.sh
@@ -62,7 +62,7 @@ sed2 _LD_FLAGS "-Wl,-O3,--sort-common,--as-needed"
 sed2 _CROSS_FLAGS "-O3 -pipe -msse3 -mfpmath=sse -ftree-vectorize -Wno-error=implicit-function-declaration -Wno-error=incompatible-pointer-types"
 sed2 _CROSS_LD_FLAGS "-Wl,-O3,--sort-common,--as-needed"
 sed2 _NOLIB32 wow64
-sed2 _configure_userargs64 "--disable-winemenubuilder --disable-win16 --disable-tests --without-capi --without-coreaudio --without-cups --without-gphoto --without-osmesa --without-oss --without-pcap --without-pcsclite --without-sane --without-udev --without-unwind --without-usb --without-v4l2 --without-wayland --without-xinerama --without-piper"
+sed2 _configure_userargs64 "--disable-win16 --disable-tests --without-capi --without-coreaudio --without-cups --without-gphoto --without-osmesa --without-oss --without-pcap --without-pcsclite --without-sane --without-udev --without-unwind --without-usb --without-v4l2 --without-wayland --without-xinerama --without-piper"
 sed2 _user_patches_no_confirm true
 
 echo "TkG Configuration file setting completed！"
