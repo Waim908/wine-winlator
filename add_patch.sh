572cdc86a605d24d93a6719ac57a42137464f1b2
Waim908 <waimxj449@foxmail.com>
2026-03-19 20:41:03 +0800

    No mmv

--- add_patch.sh
+++ add_patch.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
+
 script_dir="$(dirname $(readlink -f "$0"))"
 source "$script_dir/$1/$2/__patch__.conf"
-echo ${_patch_file_[@]}
-for i in ${_patch_file_[@]}; do
-  ls "$script_dir/$1/$2/"$i
-  cp "$script_dir/$1/$2/"$i $3
+
+echo "${_patch_file_[@]}"
+
+for i in "${_patch_file_[@]}"; do
+  ls "$script_dir/$1/$2/$i"
+  cp "$script_dir/$1/$2/$i" "$3"
 done
-cd $3
-mmv "*.patch" "#1.mylatepatch"
+
+cd "$3" || exit 1
+
+# --- 替换开始 ---
+# 原命令: mmv "*.patch" "#1.mylatepatch"
+for f in *.patch; do
+  [ -e "$f" ] || continue
+  mv -- "$f" "${f%.patch}.mylatepatch"
+done
+# --- 替换结束 ---
