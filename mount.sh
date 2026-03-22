768d488aebfad9826080f699485f1adf1cf691cb
Waim908 <waimxj449@foxmail.com>
2026-03-22 23:57:44 +0800

    fix path

--- mount.sh
+++ mount.sh
@@ -4,10 +4,12 @@ case $1 in
     sudo mount --bind /proc $2/proc
     sudo mount --bind /sys $2/sys
     sudo mount --bind /dev $2/dev
+    sudo mount --bind /tmp $2/tmp
   ;;
   unmount)
     sudo umount -l $2/dev
     sudo umount -l $2/sys
     sudo umount -l $2/proc
+    sudo umount -l $2/tmp
   ;;
 esac
