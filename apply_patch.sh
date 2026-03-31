6607c586392b31d457deff0a64838b45aaed107f
Waim908 <waimxj449@foxmail.com>
2026-03-31 13:09:34 +0800

    update

--- apply_patch.sh
+++ apply_patch.sh
@@ -7,10 +7,18 @@ echo "${_patch_file_[@]}"
 
 cd $3 || exit 1
 
-for i in "${_patch_file_[@]}"; do
-  echo "应用补丁： $i"
-  [[ -f "$script_dir/$1/$2/$i" ]] || { echo "补丁不存在!" && exit 1;}
-  patch -p1 < "$script_dir/$1/$2/$i" || { echo "补丁应用失败!" && exit 1;}
-done
+if [[ $dryRun == 1 ]]; then
+  for i in "${_patch_file_[@]}"; do
+    echo "测试补丁： $i"
+    [[ -f "$script_dir/$1/$2/$i" ]] || { echo "补丁不存在!" && exit 1;}
+    patch --dry-run -p1 < "$script_dir/$1/$2/$i" || { echo "补丁应用失败!";}
+  done
+else
+  for i in "${_patch_file_[@]}"; do
+    echo "应用补丁： $i"
+    [[ -f "$script_dir/$1/$2/$i" ]] || { echo "补丁不存在!" && exit 1;}
+    patch -p1 < "$script_dir/$1/$2/$i" || { echo "补丁应用失败!" && exit 1;}
+  done
+fi
 cd -
 
