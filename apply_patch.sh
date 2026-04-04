0066e5cda275baa59db8221d108ab727189242f1
Waim908 <waimxj449@foxmail.com>
2026-04-04 22:52:35 +0800

    Update

--- apply_patch.sh
+++ apply_patch.sh
@@ -11,16 +11,18 @@ else
   cd $3 || exit 1
 fi
 
+[[ -z $patch_dir ]] && patch_dir="$script_dir/$1/$2"
+
 if [[ $dryRun == 1 ]]; then
   for i in "${_patch_file_[@]}"; do
     echo "测试补丁： $i"
-    [[ -f "$script_dir/$1/$2/$i" ]] || { echo "补丁不存在!" && exit 1;}
+    [[ -f "$patch_dir/$i" ]] || { echo "补丁不存在!" && exit 1;}
     patch --dry-run -p1 < "$script_dir/$1/$2/$i" || { echo "补丁测试失败!可能存在补丁先后性问题,测试结果仅供参考";}
   done
 else
   for i in "${_patch_file_[@]}"; do
     echo "应用补丁： $i"
-    [[ -f "$script_dir/$1/$2/$i" ]] || { echo "补丁不存在!" && exit 1;}
+    [[ -f "$patch_dir/$i" ]] || { echo "补丁不存在!" && exit 1;}
     patch --no-backup-if-mismatch -N -p1 < "$script_dir/$1/$2/$i" || { echo "补丁应用失败!" && exit 1;}
   done
 fi
